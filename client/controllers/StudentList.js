5ce4aa584e532df44f3aa1a06317b3853eebee57
ccmehk <ccmehk@gmail.com>
2015-10-17 05:07:44 +0800

    Add student function

--- client/controllers/StudentList.js
+++ client/controllers/StudentList.js
@@ -2,11 +2,21 @@ angular.module("ClassRoom").controller("StudentsCtrl", ['$scope', '$meteor',
     function($scope, $meteor){
 
         $scope.$meteorSubscribe('users').then(function(subscriptionHandle){
-          $scope.users = $meteor.collection(Meteor.users);
+          $scope.users = $meteor.collection(Meteor.users, false);
         });
 
         $scope.save = function(){
           $scope.users.save();
+        };
+
+        $scope.createStu = function(newStu){
+          Accounts.createUser({username: newStu.username, password: newStu.password}, function(err){
+            if(!err){
+              var createdUser = $meteor.object(Meteor.users, {username: newStu.username}, false);
+              console.log(createdUser);
+              Meteor.users.update(createdUser._id, {$set: {dob: newStu.dob, isTeacher:0}});
+            }
+          });
         };
     }
 ]);
--- server/collections/students.js
+++ server/collections/students.js
@@ -1,3 +1,5 @@
 Meteor.publish('users', function(){
   return Meteor.users.find({}, {fields: {username: 1}})
 });
+
+Accounts.createUser({username: 'Android1', password: 'password', dob:'sdjkad', type: 1});

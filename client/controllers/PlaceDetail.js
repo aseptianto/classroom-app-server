dc79b6c957b39d0c1bf4d2e2722ffb8ded2cad7a
Andrio <andrioseptianto@gmail.com>
2015-10-27 19:42:34 +0800

    Fixed question to refer to Mongo.ObjectId, not the string.

--- client/controllers/PlaceDetail.js
+++ client/controllers/PlaceDetail.js
@@ -2,26 +2,26 @@ angular.module("ClassRoom").controller("PlaceDetailCtrl", ['$scope', '$statePara
     function ($scope, $stateParams, $meteor) {
         $scope.placeId = $stateParams.placeId;
 
-        $meteor.subscribe("places").then(function(){
+        $meteor.subscribe("places").then(function () {
             $scope.place = $meteor.object(Place, new Mongo.ObjectID($scope.placeId));
         });
 
         /*
-        we might need to do a change on the database
-        location might be:
-        location: {
-            center: {
-                latitude: 22.319149,
-                longitude: 114.155440
-            },
-            zoom: 15,
-            events: {}
-        }
+         we might need to do a change on the database
+         location might be:
+         location: {
+         center: {
+         latitude: 22.319149,
+         longitude: 114.155440
+         },
+         zoom: 15,
+         events: {}
+         }
          */
 
-        $scope.getGoogleMapsCoordinates = function(){
+        $scope.getGoogleMapsCoordinates = function () {
             console.log($scope.place);
-            if(typeof $scope.place.location === "undefined") return $scope.map.center;
+            if (typeof $scope.place.location === "undefined") return $scope.map.center;
             var coordinates = {
                 latitude: $scope.place.location[0],
                 longitude: $scope.place.location[1]
@@ -52,8 +52,8 @@ angular.module("ClassRoom").controller("PlaceDetailCtrl", ['$scope', '$statePara
             marker: {
                 options: {draggable: true},
                 events: {
-                    dragend: function (marker, eventName, args){
-                        if(!$scope.place.location){
+                    dragend: function (marker, eventName, args) {
+                        if (!$scope.place.location) {
                             $scope.place.location = {};
                         }
 
@@ -64,31 +64,33 @@ angular.module("ClassRoom").controller("PlaceDetailCtrl", ['$scope', '$statePara
             }
         };
 
-        $scope.save = function(){
+        $scope.save = function () {
             $scope.place.save();
         };
 
-        $meteor.subscribe("questions").then(function(){
-            $scope.questions = $meteor.collection(function(){
-                return Question.find({place: $scope.placeId});
+        $meteor.subscribe("questions").then(function () {
+            $scope.questions = $meteor.collection(function () {
+                return Question.find({place: new Mongo.ObjectID($scope.placeId)});
             }, false);
         });
 
-        $scope.addQ = function(newQ){
+        $scope.addQ = function (newQ) {
 
             var inputData = {
-                place: $scope.placeId,
+                place: new Mongo.ObjectID($scope.placeId),
                 prompt: newQ.prompt,
                 choices: {
-                    'type':newQ.type,
-                    'data':[]
+                    'type': newQ.type,
+                    'data': []
                 },
                 answer: newQ.answer
             };
-            if(newQ.type == 0){
-                inputData.choices.data = [newQ.choiceA,newQ.choiceB,newQ.choiceC,newQ.choiceD];
+            if (newQ.type == 0) {
+                inputData.choices.data = [newQ.choiceA, newQ.choiceB, newQ.choiceC, newQ.choiceD];
             }
-          Question.insert(inputData);
+            var questionId = Question.insert(inputData);
+            console.log(questionId);
+            Place.update({_id: new Mongo.ObjectID($scope.placeId)}, {$push: {questions: new Mongo.ObjectID(questionId._str)}});
         };
     }
 ]);

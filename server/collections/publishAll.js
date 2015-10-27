63251649f0ad64a665c09e7cbfa1aee729327a1a
Andrio <andrioseptianto@gmail.com>
2015-10-27 23:53:37 +0800

    Fixed some id problems, everything except user should refer to object id. Added additional publish arguments.

--- server/collections/publishAll.js
+++ server/collections/publishAll.js
@@ -1,19 +1,39 @@
-Meteor.publish('places', function(){
+Meteor.publish('places', function(userId){
+    if(userId){
+        var session = Session.find({students: {$in:[userId]}}).fetch();
+        var placeIds = session[0].places; //just do it lol
+       // return Place.find({_id: { $in: new Mongo.ObjectID(placeIds) }} );
+        return Place.find({_id: { $in: placeIds }});
+    }
     return Place.find();
 });
 
-Meteor.publish('sessions', function(){
-  return Session.find();
+Meteor.publish('sessions', function(userId){
+    if(userId){
+        return Session.find({students: {$in:[userId]}});
+    }
+    return Session.find();
 });
 
 Meteor.publish('users', function(){
     return Meteor.users.find();
-})
+});
 
-Meteor.publish("submissions", function(){
+Meteor.publish("submissions", function(userId){
+    if(userId){
+        return Submissions.find({student: {$in: [userId]}});
+    }
     return Submissions.find();
 });
 
-Meteor.publish("questions", function(){
+Meteor.publish("questions", function(userId){
+    if(userId){
+        var session = Session.find({students: {$in:[userId]}}).fetch();
+        var placeIds = session[0].places; //just do it lol
+        //var places = Place.find({_id: { $in: placeIds }}, {_id:1}).fetch();
+        //console.log(places);
+       // console.log(Question.find({place: {$in:placeIds}}).fetch());
+        return Question.find({place: {$in:placeIds}});
+    }
     return Question.find();
 });
